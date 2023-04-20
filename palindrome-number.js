b91d811c40198bb93751bf6d805dd5f89f303aba
masa <masamaruna@gmail.com>
2023-04-20 21:06:57 +0200

    initial chaos

---
+++ palindrome-number.js
@@ -0,0 +1,16 @@
+
+const isPalindrome = function(x) {
+    while () {
+
+    }
+
+    // const jedinica = x % 10;
+    // const desetica = Math.trunc(x/10)%10;
+    // const stotica = Math.trunc(x/100)%10;
+    // console.log(jedinica);
+    // console.log(desetica);
+    // console.log(stotica);
+};
+
+const x = 12345;
+console.log(isPalindrome(x));
